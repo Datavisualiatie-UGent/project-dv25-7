f36603689a69daa40c4ec469f582ec51e292effb
Jozef Jankaj <jozef.jankaj@ugent.be>
2025-05-19 11:05:56 +0200

    revert: pull.sh

--- dataset/pull.sh
+++ dataset/pull.sh
@@ -2,7 +2,7 @@
 
 # pull in the dataset
 wget https://themessik.nl/moves_tree.json -O dataset/moves_tree.json
-#wget https://themessik.nl/winning_moves_tree.json -O dataset/winning_moves_tree.json
+wget https://themessik.nl/winning_moves_tree.json -O dataset/winning_moves_tree.json
 
 
 
